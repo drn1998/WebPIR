391f58fe204ee53c714154e66d20d0b152be10f2
Darius Runge <71567459+drn1998@users.noreply.github.com>
2022-04-02 01:28:04 +0200

    Support format 'htmldownload'
    User can now select to download the HTML table instead of viewing in the browser. File name is fixed at the moment

--- src/request.php
+++ src/request.php
@@ -5,6 +5,12 @@ require('processors/csvToHtmlTable.php');
 require('processors/htmlTableToMarqueeTable.php');
 require('processors/csvFilterRowsByPIRcode.php');
 
+if($_POST["format"] == "htmldownload") {
+	$filename = 'table.html';
+	header('Content-disposition: attachment; filename=' . $filename);
+	header('Content-type: text/html');
+}
+
 $csv = wikidataGetCsvFromSparql($_POST["sparql"]);
 
 if(isset($_POST["pir"]))
