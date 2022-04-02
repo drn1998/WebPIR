901d75444108c9080237c310a92b64ee029b4be9
Darius Runge <dariusrunge@hotmail.com>
2022-04-02 14:20:07 +0200

    Consistent style for echo processor

--- src/processors/htmlTableToMarqueeTable.php
+++ src/processors/htmlTableToMarqueeTable.php
@@ -18,13 +18,11 @@ function htmlTableToMarqueeTable($html, $scroll_speed) {
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     </head>
     <body>
-    <marquee direction="up" style="position: fixed" height="100%" width="100%" scrollamount="' . $scroll_speed . '"><center><h1 style="color:red; font-family:sans-serif">Watch entirely and with covered webcam; avoid moving mouse or touching screen!</h1>
-    ');
-        
-    echo($html);
+    <marquee direction="up" style="position: fixed" height="100%" width="100%" scrollamount="' . $scroll_speed . '"><center><h1 style="color:red; font-family:sans-serif">Watch entirely and with covered webcam; avoid moving mouse or touching screen!</h1>' .
 
-    echo('
-    <h1 style="color:green; font-family:sans-serif">End of table. If you decide to watch again, watch completely!</h1></center></marquee>
+    $html
+
+    . '<h1 style="color:green; font-family:sans-serif">End of table. If you decide to watch again, watch completely!</h1></center></marquee>
     </body>
     </html>
     ');
