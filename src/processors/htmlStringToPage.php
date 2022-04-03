b8ad0634d3e7d519a6cfc23cf2b1ab0d3352f1e4
Darius Runge <dariusrunge@hotmail.com>
2022-04-03 12:57:49 +0200

    Processor for page (for manual PDF printing if supported by Browser)

---
+++ src/processors/htmlStringToPage.php
@@ -0,0 +1,26 @@
+<?php
+
+function htmlStringToPage($html, $title) {
+    echo('
+    <!DOCTYPE html>
+    <html>
+        <head>
+            <title>Laufschrift-Beispiel</title>
+            <meta name="viewport" content="width=device-width, initial-scale=1, user-scalable=no">
+            <meta charset="utf-8"/>
+            <style>
+            h1, p, marquee {
+                font-family: sans-serif;
+            }
+            </style>
+        </head>
+        <body><p>' .
+        $title
+        . '</p><p>' .
+            $html
+            . '</p>
+        </body>
+        </html>');
+}
+
+?>
