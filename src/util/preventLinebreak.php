19abc5961bf330a6c8e9902d8ba7f53d285f4e5b
Darius Runge <dariusrunge@hotmail.com>
2022-04-03 18:07:03 +0200

    Consecutive calls

--- src/util/preventLinebreak.php
+++ src/util/preventLinebreak.php
@@ -2,7 +2,7 @@
 
 function preventLinebreak($string) {
   $output = str_replace(' ', '&nbsp;', $string);
-  $output = str_replace('-', '&#8209;', $string);
+  $output = str_replace('-', '&#8209;', $output);
 
   return $output;
 }
