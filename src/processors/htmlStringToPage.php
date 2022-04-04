a03c4f09a6100cb759925ba4b7e8173d69e05213
Darius Runge <dariusrunge@hotmail.com>
2022-04-04 21:03:25 +0200

    Tables also sans serif and smaller

--- src/processors/htmlStringToPage.php
+++ src/processors/htmlStringToPage.php
@@ -9,7 +9,7 @@ function htmlStringToPage($html, $title) {
             <meta name="viewport" content="width=device-width, initial-scale=1, user-scalable=no">
             <meta charset="utf-8"/>
             <style>
-            p {
+            p, td {
                 font-family: sans-serif;
                 font-size: smaller;
             }
