3d6d9e02d1cee4456755e6a8b2f186597f1f18cd
Darius Runge <dariusrunge@hotmail.com>
2022-04-04 16:57:11 +0200

    Test for encode

---
+++ src/test/encode.php
@@ -0,0 +1,21 @@
+<?php
+
+require_once("../encode.php");
+
+function test_encode($input, $expected_output) {
+  $real_output = encode($input);
+
+  if($real_output == $expected_output)
+    echo("Passed!\n");
+  else
+    echo("Not passed!\n");
+}
+
+test_encode("", "");
+test_encode("o", "75");
+test_encode("O", "75");
+test_encode("AbC", "51963");
+test_encode("simple test", "436976681831483");
+test_encode("Crêpe", "963781761");
+
+?>
