86d726ede998604aef2e84a994a946cdbfd0840d
Darius Runge <dariusrunge@hotmail.com>
2022-04-11 18:06:09 +0200

    Systematic obtaining of escaped config data

--- src/processors/csvFilterRowsByPIRcode.php
+++ src/processors/csvFilterRowsByPIRcode.php
@@ -7,13 +7,17 @@ function csvFilterRowsByPIRcode($csv, $pir, $format)
     $output = "";
     $conf   = parse_ini_file("config/" . $format . ".ini", true);
 
-    $rows = explode(stripcslashes($conf["csv"]["line"]), $csv);
+    $line_sep  = stripcslashes($conf["csv"]["line"]);
+    $field_sep = stripcslashes($conf["csv"]["field"]);
+    $field_exc = stripcslashes($conf["csv"]["except"]);
+
+    $rows = explode($line_sep, $csv);
 
     foreach ($rows as &$row) {
-        $cells = str_getcsv($row, stripcslashes($conf["csv"]["field"]), stripcslashes($conf["csv"]["except"]));
+        $cells = str_getcsv($row, $field_sep, $field_exc);
 
         if (pir_satisfied($cells[0], $pir) == true)
-            $output .= $row . stripcslashes($conf["csv"]["line"]);
+            $output .= $row . $line_sep;
     }
 
     return $output;
--- src/processors/csvToHtmlString.php
+++ src/processors/csvToHtmlString.php
@@ -6,12 +6,17 @@ function csvToHtmlString($csv, $format)
 {
     $body = "";
     $conf = parse_ini_file("config/" . $format . ".ini", true);
-    $rows = explode(stripcslashes($conf["csv"]["line"]), $csv);
+
+    $line_sep  = stripcslashes($conf["csv"]["line"]);
+    $field_sep = stripcslashes($conf["csv"]["field"]);
+    $field_exc = stripcslashes($conf["csv"]["except"]);
+
+    $rows = explode($line_sep, $csv);
 
     $body .= "<p>";
 
     foreach ($rows as &$row) {
-        $cells = str_getcsv($row, stripcslashes($conf["csv"]["field"]), stripcslashes($conf["csv"]["except"]));
+        $cells = str_getcsv($row, $field_sep, $field_exc);
         foreach ($cells as &$cell) {
             if ($cell == "")
                 $cell = "&varnothing;";
@@ -30,11 +35,11 @@ function csvToHtmlTitleString($csv, $format)
 {
     $body = "";
     $conf = parse_ini_file("config/" . $format . ".ini", true);
-    $rows = explode(stripcslashes($conf["csv"]["line"]), $csv);
+    $rows = explode($line_sep, $csv);
 
     $body .= "<strong>";
 
-    $body .= implode(", ", str_getcsv($rows[0], stripcslashes($conf["csv"]["field"]), stripcslashes($conf["csv"]["except"])));
+    $body .= implode(", ", str_getcsv($rows[0], $field_sep, $field_exc));
 
     $body .= "</strong>";
 
